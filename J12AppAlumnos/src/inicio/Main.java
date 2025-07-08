0cfc0018e260d0040a44c5989d61213a6940ccdf
plopezgrieco <plopezgrieco@gmail.com>
2025-07-08 21:31:20 +0200

    inicial

--- J12AppAlumnos/src/inicio/Main.java
+++ J12AppAlumnos/src/inicio/Main.java
@@ -1,11 +1,10 @@
 package inicio;
 
-import vista.MenuPrincipal;
 import vista.swing.VInicial;
 
 public class Main {
 	public static void main(String[] args) {
-		new MenuPrincipal();
-//		new VInicial();
+//		new MenuPrincipal();
+		new VInicial();
 	}
 }
--- J12AppAlumnos/src/negocio/GestionAlumnosImpl.java
+++ J12AppAlumnos/src/negocio/GestionAlumnosImpl.java
@@ -3,6 +3,8 @@ package negocio;
 import java.util.Collection;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import modelo.Alumno;
 import persistencia.AlumnoDao;
@@ -18,19 +20,23 @@ public class GestionAlumnosImpl implements GestionAlumnos {
 	
 	@Override
 	public Set<Alumno> getAprobados() {
-		Set<Alumno> aprobados = new TreeSet<Alumno>(Alumno.ordenAlfabetico());
-		for (Alumno alumno : dao.findAll()) {
-			if (alumno.getNota() >= 5)
-				aprobados.add(alumno);
-		}
-		return aprobados;
+//		Set<Alumno> aprobados = new TreeSet<Alumno>(Alumno.ordenAlfabetico());
+//		for (Alumno alumno : dao.findAll()) {
+//			if (alumno.getNota() >= 5)
+//				aprobados.add(alumno);
+//		}
+//		return aprobados;
+		
+		return getAlumnosFiltro(a -> a.getNota() >= 5);
 	}
 
+
 	private double getMediaNotasFromCollection(Collection<Alumno> alumnos) {
-		double suma = 0;
-		for (Alumno alumno : alumnos)
-			suma += alumno.getNota();
-		return suma / alumnos.size();
+//		double suma = 0;
+//		for (Alumno alumno : alumnos)
+//			suma += alumno.getNota();
+//		return suma / alumnos.size();
+		return alumnos.stream().mapToDouble(a -> a.getNota()).average().orElse(0);
 	}
 	
 	@Override
@@ -68,44 +74,70 @@ public class GestionAlumnosImpl implements GestionAlumnos {
 		dao.delete(alumno);
 	}
 
+//	@Override
+//	public Set<Alumno> getSB() {
+//		Set<Alumno> resu = new TreeSet<Alumno>(Alumno.ordenAlfabetico());
+//		for (Alumno alumno : dao.findAll()) {
+//			if (alumno.getNota() >= 9)
+//				resu.add(alumno);
+//		}
+//		return resu;
+//	}
+//
+//	@Override
+//	public Set<Alumno> getNT() {
+//		Set<Alumno> resu = new TreeSet<Alumno>(Alumno.ordenAlfabetico());
+//		for (Alumno alumno : dao.findAll()) {
+//			if (alumno.getNota() >= 8 && alumno.getNota() < 9)
+//				resu.add(alumno);
+//		}
+//		return resu;
+//	}
+//
+//	@Override
+//	public Set<Alumno> getAP() {
+//		Set<Alumno> resu = new TreeSet<Alumno>(Alumno.ordenAlfabetico());
+//		for (Alumno alumno : dao.findAll()) {
+//			if (alumno.getNota() >= 5 && alumno.getNota() < 8)
+//				resu.add(alumno);
+//		}
+//		return resu;
+//	}
+//
+//	@Override
+//	public Set<Alumno> getIN() {
+//		Set<Alumno> resu = new TreeSet<Alumno>(Alumno.ordenAlfabetico());
+//		for (Alumno alumno : dao.findAll()) {
+//			if (alumno.getNota() < 5)
+//				resu.add(alumno);
+//		}
+//		return resu;
+//	}
+	
+	private Set<Alumno> getAlumnosFiltro(Predicate<Alumno> p) {
+//		return dao.findAll().stream().filter(p).collect(Collectors.toCollection(TreeSet::new));
+		return dao.findAll().stream().filter(p).collect(Collectors.toCollection(() -> new TreeSet<Alumno>(Alumno.ordenAlfabetico())));
+	}
+
 	@Override
 	public Set<Alumno> getSB() {
-		Set<Alumno> resu = new TreeSet<Alumno>(Alumno.ordenAlfabetico());
-		for (Alumno alumno : dao.findAll()) {
-			if (alumno.getNota() >= 9)
-				resu.add(alumno);
-		}
-		return resu;
+		return getAlumnosFiltro(a -> a.getNota() >= 9);
 	}
 
 	@Override
 	public Set<Alumno> getNT() {
-		Set<Alumno> resu = new TreeSet<Alumno>(Alumno.ordenAlfabetico());
-		for (Alumno alumno : dao.findAll()) {
-			if (alumno.getNota() >= 8 && alumno.getNota() < 9)
-				resu.add(alumno);
-		}
-		return resu;
+		return getAlumnosFiltro(a -> a.getNota() >= 8 && a.getNota() < 9);
 	}
 
 	@Override
 	public Set<Alumno> getAP() {
-		Set<Alumno> resu = new TreeSet<Alumno>(Alumno.ordenAlfabetico());
-		for (Alumno alumno : dao.findAll()) {
-			if (alumno.getNota() >= 5 && alumno.getNota() < 8)
-				resu.add(alumno);
-		}
-		return resu;
+		// TODO Auto-generated method stub
+		return getAlumnosFiltro(a -> a.getNota() >= 5 && a.getNota() < 8);
 	}
 
 	@Override
 	public Set<Alumno> getIN() {
-		Set<Alumno> resu = new TreeSet<Alumno>(Alumno.ordenAlfabetico());
-		for (Alumno alumno : dao.findAll()) {
-			if (alumno.getNota() < 5)
-				resu.add(alumno);
-		}
-		return resu;
+		return getAlumnosFiltro(a -> a.getNota() < 5);
 	}
 
 }
